62385bbef9454a285e0d479909f43a031e575278
SomeoneCodes <msr.23cs8059@nitdgp.ac.in>
2026-08-17 18:57:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hi-Decor/',
   plugins: [react()],
-  server: {port:5173}
+  server: { port: 5173 }
 })
